3d36036d7a950f8577d762a15cc620490b1055e5
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2024-10-28 17:07:24 +0530

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import Layout from "@/components/Layout";
 import Hero from "@/components/HomePage/Hero";
-import AboutUs from "@/components/HomePage/AboutUs";
-import Portfolio from "@/components/HomePage/Portfolio";
-import Services from "@/components/HomePage/Services";
-import Clients from "@/components/HomePage/Clients";
-import Blogs from "@/components/HomePage/Blogs";
+// import AboutUs from "@/components/HomePage/AboutUs";
+// import Portfolio from "@/components/HomePage/Portfolio";
+// import Services from "@/components/HomePage/Services";
+// import Clients from "@/components/HomePage/Clients";
+// import Blogs from "@/components/HomePage/Blogs";
 import { getHomePagePosts } from "@/lib/posts";
 import { titleAnim, paraAnim, lineAnim, fadeIn, fadeUp } from '@/components/gsapAnimations';
 import { WebpageJsonLd } from "@/lib/json-ld";
@@ -34,11 +34,11 @@ export default function Home({ recentPosts }) {
       <WebpageJsonLd metadata={metadata} />
       <Layout>
         <Hero />
-        <AboutUs />
+        {/* <AboutUs />
         <Portfolio />
         <Services />
         <Clients />
-        <Blogs posts={recentPosts} />
+        <Blogs posts={recentPosts} /> */}
       </Layout>
     </>
   );
