bcb6a962c1b1898145bc099ccec5a8b81eb84533
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-11-19 16:59:23 +0530

    update career-detail

--- src/components/career-detail/Pagehero.jsx
+++ src/components/career-detail/Pagehero.jsx
@@ -7,7 +7,7 @@ export default function Pagehero() {
     <>
       <Section id='hero' className='dark bg-white'>
         <div className='w-[100vw] h-[80vh] relative tablet:h-[40vh]'>
-          <Image src="/assets/images/career/career-detail-hero.png" alt={`Career Detail Image`} quality={100} fill className='object-cover brightness-50 h-full w-full mobile:object-cover' priority/>
+          <Image src="/assets/images/career/career-detail-hero.png" alt={`Career Detail Image`} quality={90} fill className='object-cover brightness-50 h-full w-full mobile:object-cover' priority={true}/>
           <div className='container absolute top-[50%] translate-y-[-50%] z-[2] mobile:top-[70%] tablet:top-[70%]'>
             <div className='text-white'>
               <h1 data-title-anim className='text-[5.7vw] uppercase font-display mb-[1vw] mobile:text-[10vw] mobile:leading-[1.3] mobile:mb-[7vw] tablet:text-[6vw]'>
