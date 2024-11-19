48990aea48da7fea6fb7d1d1403f505e22673aa4
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-11-19 12:44:47 +0530

    try

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home({ recentPosts }) {
         <Hero />
         <AboutUs />
         <Portfolio />
-        <Services />
+        {/* <Services /> */}
         <Clients />
         <Blogs posts={recentPosts} />
       </Layout>
