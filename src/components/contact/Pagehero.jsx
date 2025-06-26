144093a9930139521c21e7317ce5ecce8dd36e89
Vidushi Saxena <vidushisaxenabdn786@gmail.com>
2025-06-26 19:35:57 +0530

    update

--- src/components/contact/Pagehero.jsx
+++ src/components/contact/Pagehero.jsx
@@ -7,25 +7,27 @@ export default function Pagehero() {
       <section className="w-full h-full pt-[10%] flex items-center mobile:h-full mobile:pt-[30%] tablet:h-full  tablet:pt-[15%] bg-white " id="hero">
         <div className="flex justify-between container mobile:flex-col  mobile:gap-[15vw] ">
           <div className="w-full">
-            <h1 data-para-anim className="text-[5.8vw] font-display uppercase w-[70%] leading-[1.1] mb-[3vw] mobile:text-[12vw] mobile:w-full mobile:mb-[7vw] tablet:text-[7vw] tablet:w-[80%]">Hey There ! Good to See you !</h1>
-            <div className="w-[70%] font-medium text-[1.4vw] space-y-[1.5vw] mobile:text-[5vw] mobile:w-full tablet:text-[2.2vw] tablet:w-[80%] ">
+            <h1 data-para-anim className="text-[5.5vw] font-display uppercase w-[92%] leading-[1.1] mb-[3vw] mobile:text-[12vw] mobile:w-full mobile:mb-[7vw] tablet:text-[7vw] tablet:w-[80%]">Hey There ! <br/> Good to See you !</h1>
+            <div className="w-[80%] text-[1.18vw] space-y-[1vw] mobile:text-[5vw] mobile:w-full tablet:text-[2.2vw] tablet:w-[80%] ">
               <p data-para-anim>Let&apos;s Build Something Bright Together!</p>
-              <p data-para-anim>Looking for a branding agency in Dubai that acts like a co-founder? You&apos;re in the right place. At Yellow, we help businesses stand out, scale up, and stay relevant - through strategy, design, marketing, and everything in between.
+              <p data-para-anim>Looking for a branding agency in Dubai that acts like a co-founder? You&apos;re in the right place. At Yellow, we help businesses stand out, scale up, and stay relevant - through <span className="underline"> strategy </span>, <span className="underline"> design </span>, <span className="underline"> marketing </span>, and everything in between.
               </p>
               <p data-para-anim>Whether you&apos;re launching a new brand, refreshing your legacy brand, or simply need a creative partner who speaks your language and is fully invested, let&apos;s get in touch.</p>
+              <div>
               <p data-para-anim>What we can help with:</p>
-              <ul className="list-disc ml-[2vw]">
+              <ul className="list-disc ml-[2vw] pt-[0.5vw] font-medium">
                 <li>Brand strategy & naming</li>
                 <li>Visual identity design & rebranding</li>
                 <li>Website design</li>
                 <li>Social media & content creation</li>
                 <li>Digital marketing & launch campaigns</li>
               </ul>
+              </div>
               <p data-para-anim>Tell us what you need - or let&apos;s workshop and figure it out together.</p>
               <p data-para-anim>Based in Dubai, working with ambitious brands across the region and beyond.</p>
             </div>
           </div>
-          <div className="w-[50vw] h-[40vw] relative mobile:w-[85vw] mobile:h-[120vw] rounded-xl overflow-hidden fadeup">
+          <div className="w-[50vw] h-[44vw] relative mobile:w-[85vw] mobile:h-[120vw] rounded-xl overflow-hidden fadeup">
             <div>
               <Image src="/assets/images/contact/contact-head2.webp" alt="contac-head-img" className="object-cover h-full w-full " fill priority={true} />
             </div>
